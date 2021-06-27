c97cb916d0ba99739ab9dc76100db47d88dc9fe9
artem <makarenkoiandeks@yandex.ru>
2021-06-27 13:39:27 +0400

    v1.3

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
 from requests import get, post
-import json
 
 try:
     import vk_api
@@ -19,8 +17,6 @@ except ModuleNotFoundError:
 
     pip.main(["install", "fuzzywuzzy"])
     from fuzzywuzzy import fuzz, process
-import datetime
-from time import sleep
 from config import *
 
 login, password = VK_LOGIN, VK_PASSWORD
@@ -32,58 +28,45 @@ vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session_bot, wait=1)
 longpoll_my = VkLongPoll(vk_session, wait=1)
 
-
-def send_messages(chat_id, text, attachment=''):
-    random_id = random.randint(0, 1000)
-    try:
-        if not len(attachment):
-            return vk_bot.messages.send(chat_id=chat_id, message=text, random_id=random_id)
-        else:
-            return vk_bot.messages.send(chat_id=chat_id, message=text, random_id=random_id,
-                                        attachment=attachment)
-    except vk_api.exceptions.ApiError as e:
-        print(f'Error {e.__class__.__name__}: {e}')
-
-
 gif_url = "http://api.giphy.com/v1/gifs/search"
-
 me_in_chat, me = None, None
 while 1:
     for event in longpoll.check():
-        if event.from_chat and not event.from_me and (
-                event.type == VkEventType.MESSAGE_NEW or event.type == VkEventType.MESSAGE_EDIT):
-            print(vars(event))
-            print(event.from_chat, event.from_me, event.type == VkEventType.MESSAGE_NEW, event.type == VkEventType.MESSAGE_EDIT)
+        print(event.from_chat, event.to_me, event.type == VkEventType.MESSAGE_NEW, event.type == VkEventType.MESSAGE_EDIT)
+        if event.from_chat and event.to_me and (
+                event.type == VkEventType.MESSAGE_NEW or event.type == VkEventType.MESSAGE_EDIT) and len(event.text):
             if event.text[0] != ",":
-                params = {
-                    "api_key": GIF_TOKEN,
-                    "q": event.text,
-                    "limit": "1"
-                }
+                lang = "ru"
+                if 64 < ord(event.text[0]) < 123:
+                    lang = "en"
+                params = {"api_key": GIF_TOKEN, "q": event.text, "limit": "3", "lang": lang}
                 #print(vars(event))
                 #print(gif_url)
                 #print(params)
                 data = get(gif_url, params=params).json()
                 #print(data)
-                gif = data["data"][0]["images"]["fixed_height"]["url"]
-                with open('test.gif', 'wb') as file:
-                    file.write(get(gif).content)
-                '''upload_url = vk.photos.getUploadServer(group_id=205470982, album_id=279908245, v=5.95)["upload_url"]
-                post_r = post(upload_url, files={"file": open("test.gif", "rb")}).json()
-                print(post_r)
-                save = vk.photos.save(group_id=205470982, album_id=279908245, v=5.95, photos_list=post_r['photos_list'],
-                                           server=post_r['server'], hash=post_r['hash'], aid=post_r['aid'])
-                saved_photo = "gif" + str(save[0]['owner_id']) + "_" + str(save[0]['id'])
-                print(saved_photo)'''
-                upload_url = vk.docs.getUploadServer(group_id=205470982, v=5.95)["upload_url"]
-                post_r = post(upload_url, files={"file": open("test.gif", "rb")}).json()
-                #print(post_r)
-                save = vk.docs.save(v=5.95, file=post_r['file'])
-                #print(save)
-                saved_gif = "https://vk.com/doc" + str(save["doc"]['owner_id']) + "_" + str(save["doc"]['id'])
-                print(saved_gif)
-                vk_bot.messages.send(chat_id=event.chat_id, message=saved_gif,
-                                     random_id=random.randint(0, 1000))
+                for gif_file in data["data"]:
+                    print(gif_file)
+                    gif = gif_file["images"]["fixed_height"]["url"]
+                    with open('test.gif', 'wb') as file:
+                        file.write(get(gif).content)
+                    '''upload_url = vk.photos.getUploadServer(group_id=205470982, album_id=279908245, v=5.95)["upload_url"]
+                    post_r = post(upload_url, files={"file": open("test.gif", "rb")}).json()
+                    print(post_r)
+                    save = vk.photos.save(group_id=205470982, album_id=279908245, v=5.95, photos_list=post_r['photos_list'],
+                                               server=post_r['server'], hash=post_r['hash'], aid=post_r['aid'])
+                    saved_photo = "gif" + str(save[0]['owner_id']) + "_" + str(save[0]['id'])
+                    print(saved_photo)'''
+                    upload_url = vk.docs.getUploadServer(group_id=205470982, v=5.95)["upload_url"]
+                    post_r = post(upload_url, files={"file": open("test.gif", "rb")}).json()
+                    #print(post_r)
+                    save = vk.docs.save(v=5.95, file=post_r['file'])
+                    #print(save)
+                    saved_gif = "https://vk.com/doc" + str(save["doc"]['owner_id']) + "_" + str(save["doc"]['id'])
+                    #saved_gif = "https://vk.com/doc-205470982_606197486"
+                    #print(saved_gif)
+                    vk_bot.messages.send(chat_id=event.chat_id, message=saved_gif,
+                                         random_id=random.randint(0, 1000))
 
     for event in longpoll_my.check():
         if (event.type == VkEventType.MESSAGE_NEW and event.from_me) or (

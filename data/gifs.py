f912458624bd4ccdc6d2c48d3b80ae3a77f208a1
artem <makarenkoiandeks@yandex.ru>
2021-08-07 23:32:38 +0400

    v4.21

--- data/gifs.py
+++ data/gifs.py
@@ -6,7 +6,7 @@ from sqlalchemy_serializer import SerializerMixin
 class Gif(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'gifs'
 
-    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     link = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     words_en = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     words_ru = sqlalchemy.Column(sqlalchemy.String, nullable=True)

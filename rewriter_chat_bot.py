6f80cd0059e84199e334ff0458c979fcbdd5ad0c
artem <makarenkoiandeks@yandex.ru>
2021-08-16 15:35:17 +0400

    v4.25(+timer)

--- rewriter_chat_bot.py
+++ rewriter_chat_bot.py
@@ -1,6 +1,6 @@
 from requests import get, post
 from threading import Thread
-import vk_api
+import datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
 import random
 
@@ -40,17 +40,25 @@ def rewrite(event, vk):
 
 
 def main(vk, longpoll_my):
+    people_send_audio = {}
     me_in_chat, me = None, None
     for event in longpoll_my.listen():
         try:
-            #print_with_title(vars(event))
+            # print_with_title(vars(event))
             if '"type":"audio_message"' in event.attachments[
                 'attachments'] and event.type == VkEventType.MESSAGE_NEW and event.to_me and not event.from_chat:
-                vk.messages.send(peer_id=event.peer_id, message="https://vk.com/video-205470982_456239017",
+                if event.peer_id in people_send_audio.keys() and datetime.datetime.now() - people_send_audio[
+                    event.peer_id] < datetime.timedelta(minuts=30):
+                    a = 0 / 1
+                people_send_audio[event.peer_id] = datetime.datetime.now()
+                vk.messages.send(peer_id=event.peer_id, message="",
+                                 attachment="https://vk.com/video-205470982_456239017",
                                  random_id=random.randint(0, 1000))
         except BaseException as e:
-            pass#print_with_title("!!!", e.__class__, e)
-        if (event.type == VkEventType.MESSAGE_NEW and event.from_me) or (event.type == VkEventType.MESSAGE_EDIT and (
-                event.user_id == me or (event.from_chat and event.user_id == me_in_chat))):
-            t = Thread(target=rewrite, args=(event, vk,))
-            t.start()
+            pass  # print_with_title("!!!", e.__class__, e)
+        finally:
+            if (event.type == VkEventType.MESSAGE_NEW and event.from_me) or (
+                    event.type == VkEventType.MESSAGE_EDIT and (
+                    event.user_id == me or (event.from_chat and event.user_id == me_in_chat))):
+                t = Thread(target=rewrite, args=(event, vk,))
+                t.start()
